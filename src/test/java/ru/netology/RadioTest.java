b3f3e3ac2e5469394366b094b3d0a58e5f6a6b61
Alexander Siluyanov <antidot8@mail.ru>
2021-04-29 23:59:36 +0400

    Delete unused tests

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -8,24 +8,6 @@ class RadioTest {
 
     Radio radio = new Radio();
 
-//    @Test
-//    public void SetStationAboveLimit() {
-//        radio.setCurrentStation(10);
-//        assertEquals(0, radio.getCurrentStation());
-//    }
-//
-//    @Test
-//    public void SetStationUnderLimit() {
-//        radio.setCurrentStation(-1);
-//        assertEquals(0, radio.getCurrentStation());
-//    }
-//
-//    @Test
-//    public void SetStationPositiveLimit() {
-//        radio.setCurrentStation(5);
-//        assertEquals(5, radio.getCurrentStation());
-//    }
-
     @Test
     public void NextStationAboveLimit() {
         radio.setNextStation(9);
@@ -49,28 +31,4 @@ class RadioTest {
         radio.setPrevStation(5);
         assertEquals(4, radio.getCurrentStation());
     }
-
-//    @Test
-//    public void UpVolumeAboveLimit() {
-//        radio.setUpVolume(10);
-//        assertEquals(10, radio.getCurrentVolume());
-//    }
-//
-//    @Test
-//    public void DownVolumeUnderLimit() {
-//        radio.setDownVolume(0);
-//        assertEquals(0, radio.getCurrentVolume());
-//    }
-//
-//    @Test
-//    public void UpVolumePositiveLimit() {
-//        radio.setUpVolume(8);
-//        assertEquals(9, radio.getCurrentVolume());
-//    }
-//
-//    @Test
-//    public void DownVolumePositiveLimit() {
-//        radio.setDownVolume(4);
-//        assertEquals(3, radio.getCurrentVolume());
-//    }
 }
